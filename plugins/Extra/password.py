945428f30d51404ae97d0788b48d8b2ee54b7610
Divyanshu Chaudhary <Divyanshu2480@gmail.com>
2026-07-08 20:03:28 +0530

    Update password.py

--- plugins/Extra/password.py
+++ plugins/Extra/password.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot @DEGs
-# Ask Doubt on telegram @KingVJ01
-
 import random, os
 from info import CHNL_LNK
 from pyrogram import Client, filters, enums 
