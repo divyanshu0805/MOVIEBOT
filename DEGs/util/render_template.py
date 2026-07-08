b728293585c930254b822b2ac0289572f94842d8
Divyanshu Chaudhary <Divyanshu2480@gmail.com>
2026-07-08 19:32:09 +0530

    Update render_template.py

--- DEGs/util/render_template.py
+++ DEGs/util/render_template.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot @DEGs
-# Ask Doubt on telegram @KingVJ01
-
 import jinja2
 from info import *
 from DEGs.bot import DEGsBot
