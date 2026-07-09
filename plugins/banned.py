84675cc292a75af3366ca534b461bdba05a8aa74
Divyanshu Chaudhary <Divyanshu2480@gmail.com>
2026-07-10 03:11:36 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot @DEGs
-# Ask Doubt on telegram @KingVJ01
-
 from pyrogram import Client, filters
 from utils import temp
 from pyrogram.types import Message
