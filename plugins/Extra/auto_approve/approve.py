c13bec7efd5c3049d3857b24618f9d0517815fdd
Divyanshu Chaudhary <Divyanshu2480@gmail.com>
2026-07-10 12:15:56 +0530

    Update approve.py

--- plugins/Extra/auto_approve/approve.py
+++ plugins/Extra/auto_approve/approve.py
@@ -18,17 +18,6 @@ join_db = JoinReqs
 
 @Client.on_chat_join_request((filters.group | filters.channel))
 async def auto_approve(client, message: ChatJoinRequest):
-    if AUTO_APPROVE_MODE == True:
-        if not await db.is_user_exist(message.from_user.id):
-            await db.add_user(message.from_user.id, message.from_user.first_name)
-        if message.chat.id == AUTH_CHANNEL:
-            return 
-        chat = message.chat 
-        user = message.from_user  
-        await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
-        text = f"<b>ʜᴇʟʟᴏ {message.from_user.mention} 👋,\n\nʏᴏᴜʀ ʀᴇǫᴜᴇsᴛ ᴛᴏ ᴊᴏɪɴ {message.chat.title} ɪs ᴀᴘᴘʀᴏᴠᴇᴅ.\n\nᴘᴏᴡᴇʀᴇᴅ ʙʏ - {CHNL_LNK}</b>"
-        await client.send_message(chat_id=user.id, text=text)
-         
     if REQUEST_TO_JOIN_MODE == False:
         return 
     if message.chat.id != AUTH_CHANNEL:
