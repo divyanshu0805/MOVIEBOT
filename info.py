8ecfde929dff0c75f116aee316c1b6a179bfd011
Divyanshu Chaudhary <Divyanshu2480@gmail.com>
2026-07-11 12:16:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,3 +1,7 @@
+# Don't Remove Credit @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot @DEGs
+# Ask Doubt on telegram @KingVJ01
+
 import re
 from os import environ
 from Script import script 
@@ -127,7 +131,7 @@ QUALITIES = ["360p", "480p", "720p", "1080p", "1440p", "2160p"]
 YEARS = ["1900", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025"]
 
 # Online Stream and Download
-STREAM_MODE = bool(environ.get('STREAM_MODE', True))
+STREAM_MODE = bool(environ.get('STREAM_MODE', False))
 MULTI_CLIENT = False
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '120'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))
