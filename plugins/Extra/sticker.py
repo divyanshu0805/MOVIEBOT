d50cb2de90718cefe74128f5104c0a48f073a655
Divyanshu Chaudhary <Divyanshu2480@gmail.com>
2026-07-12 18:02:52 +0530

    Update sticker.py\

--- plugins/Extra/sticker.py
+++ plugins/Extra/sticker.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot @DEGs
-# Ask Doubt on telegram @KingVJ01
-
 from pyrogram import Client, filters
 
 @Client.on_message(filters.command("stickerid") & filters.private)
