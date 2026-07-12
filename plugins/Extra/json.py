18350061e8271086c6f314319316d2d2cf71f080
Divyanshu Chaudhary <Divyanshu2480@gmail.com>
2026-07-12 18:01:20 +0530

    Update json.py

--- plugins/Extra/json.py
+++ plugins/Extra/json.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot @DEGs
-# Ask Doubt on telegram @KingVJ01
-
 import os
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
