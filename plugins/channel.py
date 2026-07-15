6b0d5f4db177297ca1ff2aece07acbdde8d596aa
Divyanshu Chaudhary <Divyanshu2480@gmail.com>
2026-07-15 12:09:50 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,10 +1,22 @@
 from pyrogram import Client, filters
 from info import CHANNELS
 from database.ia_filterdb import save_file
+from database.channels_db import get_index_channels
 
 media_filter = filters.document | filters.video
 
-@Client.on_message(filters.chat(CHANNELS) & media_filter)
+
+async def index_channel_filter(_, __, message):
+    """Check karo ki ye message kisi bhi tracked channel (env var + DB dono) se hai."""
+    if message.chat.id in CHANNELS:
+        return True
+    db_channels = await get_index_channels()
+    return message.chat.id in db_channels
+
+dynamic_channels_filter = filters.create(index_channel_filter)
+
+
+@Client.on_message(dynamic_channels_filter & media_filter)
 async def media(bot, message):
     media = getattr(message, message.media.value, None)
     media.caption = message.caption
